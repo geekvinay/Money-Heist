10ffeccbe7b0596383353574391cc9ce9838bf52
geekvinay <geekvinay002@gmail.com>
2021-12-30 12:58:01 +0530

    added many more changes

--- main.js
+++ main.js
@@ -3,19 +3,36 @@ import './styles.css'
 import 'normalize.css'
 
 let loader = document.querySelector(".loading");
-let mobileMessage = document.querySelector(".mobile-message");
-let loaderBar = document.querySelector(".loading--bar");
 let body = document.querySelector("body");
-let screenWidth = window.innerWidth;
-// console.log(screenWidth);
-// console.log(loader);
-// console.log(mobileMessage,loaderBar);
+let reviews = document.querySelector(".reviews");
+let reviewsModal = document.querySelector(".review--hero");
+let reviewsClose = document.querySelector(".fa-window-close");
+let screenwidth = window.innerWidth;
+console.log(screenwidth);
+
+reviews.addEventListener('click', function () {
+    reviewsModal.style.opacity = "1";
+    reviewsModal.style.pointerEvents = "all";
+})
+
+reviewsClose.addEventListener("click", () => {
+    reviewsModal.style.opacity = "0";
+    reviewsModal.style.pointerEvents = "none";
+})
+
 
 
 window.addEventListener('load', () => {
     setTimeout(function () {
         loader.style.opacity = "0";
-        body.style.overflowY = "scroll";
+        // body.classList.add = "overflow--scroll";
+
+
+        if (screenwidth < 800) {
+            body.style.overflowY = "hidden";
+        }
+        else
+            body.style.overflowY = "scroll";
     }, 5000)
 });
 
